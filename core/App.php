2b93471449c3d00eee7069241891bc21fa39aeb1
julbel1984 <julbel1984@yandex.ru>
2019-10-13 10:16:43 +0300

    add container DI base

---
+++ core/App.php
@@ -0,0 +1,44 @@
+<?php
+
+
+/**
+ * Базовый контейнер внедрения зависимостей DI.
+ * Действует как место привязки зависимостей, которые
+ * были отправлены ему.
+ *
+ * Class App
+ */
+class App
+{
+    /**
+     * Массив значений config.php
+     *
+     * @var array
+     */
+    protected static $registry = [];
+
+    /**
+     * Пара ключ значение
+     *
+     * @param $key
+     * @param $value
+     */
+    public static function bind($key, $value)
+    {
+        static::$registry[$key] = $value;
+    }
+
+    /**
+     * @param $key
+     * @return mixed
+     * @throws Exception
+     */
+    public static function get($key)
+    {
+        if (!array_key_exists($key, static::$registry)) {
+            throw new Exception("Нет {$key} связанных в контейнере");
+        }
+
+        return static::$registry[$key];
+    }
+}
