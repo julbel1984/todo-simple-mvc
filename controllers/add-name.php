441a8335ec645097c275a8f1f4386308094660bd
julbel1984 <julbel1984@yandex.ru>
2019-10-13 10:17:45 +0300

    update for DI

--- controllers/add-name.php
+++ controllers/add-name.php
@@ -1,5 +1,5 @@
 <?php
 
-$app['database']->insert('users', ['name'=> $_POST['name']]);
+App::get('database')->insert('users', ['name'=> $_POST['name']]);
 
 header('Location: /');
--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,8 +1,17 @@
 <?php
 
-$app = [];
-$app['config'] = require 'config.php';
+/**
+ * Создает ключ с именем config, который имеет массив config.php
+ * в качестве значения, а затем сохраняет пару ключ-значение
+ * в контейнере DI
+ */
+App::bind('config', require 'config.php');
 
-$app['database'] = new QueryBuilder(
-    Connection::make($app['config']['database'])
-);
+/**
+ * Создает ключ с именем database,
+ * значение которого является экземпляром QueryBuilder
+ * и сохраняет его в контейнере DI
+ */
+App::bind('database', new QueryBuilder(
+    Connection::make(App::get('config')['database'])
+));
